bfe17a5312713ec1896de846ff8051e35a98a8a6
Irina <izhts1482@gmail.com>
2023-10-31 14:14:27 +0300

    Branch testng added

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,29 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTest {
+    @Test
+    public void shouldCalculateAmount900() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 900;
+
+        int actual = service.remain(900);
+        int expected = 100;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldCalculateAmount100() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 100;
+
+        int actual = service.remain(100);
+        int expected = 900;
+
+        assertEquals(actual, expected);
+    }
+}
